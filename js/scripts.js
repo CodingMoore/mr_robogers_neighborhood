1ad3da75d7848191a6ab34d7009d6d44d2b57400
Randel M <Randel.C.Moore@gmail.com>
2020-10-30 14:18:27 -0700

    add connection between ui and business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,27 @@
 //Business Logic
-let input
-let inputNum = parseFloat(input);
-let outputArray = [];
-let inputArray = [];
-let inputString = [];
-let splitArray = [];
-for (let i = 0; i <= inputNum; i++) {
-  inputArray.push(i);
-  inputString = inputArray.join(" ");
-  splitArray = inputString.split(" ");
-  if (splitArray[i].includes("3")) {
-    outputArray.push("Won't you be my neighbor?")
+function mrRobo() {
+  let inputNum = parseFloat(input);
+  let outputArray = [];
+  let inputArray = [];
+  let inputString = [];
+  let splitArray = [];
+  for (let i = 0; i <= inputNum; i++) {
+    inputArray.push(i);
+    inputString = inputArray.join(" ");
+    splitArray = inputString.split(" ");
+    if (splitArray[i].includes("3")) {
+      outputArray.push("Won't you be my neighbor?")
+    }
+    else if (splitArray[i].includes("2")) {
+      outputArray.push("Boop!")
+    }
+    else if (splitArray[i].includes("1")) {
+      outputArray.push("Beep!")
+    } else outputArray.push(i)
   }
-  else if (splitArray[i].includes("2")) {
-    outputArray.push("Boop!")
-  }
-  else if (splitArray[i].includes("1")) {
-    outputArray.push("Beep!")
-  } else outputArray.push(i)
+  const result = outputArray.join(", ")
+  return result
 }
-const result = outputArray.join(", ")
 
 
 
@@ -28,9 +30,10 @@ const result = outputArray.join(", ")
 //User Interface Logic
 $(document).ready(function() {
   $("#inputForm").submit(function(event) {
-    const input = $("#numInput").val();
-    $("#outputText").text(result);
     event.preventDefault();
+    input = $("#numInput").val();
+    mrRobo(input);
+    $("#outputText").text(mrRobo());
   });
 });
-console.log(input)
+
